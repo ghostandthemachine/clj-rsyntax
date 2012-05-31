225816104e2de83498f4fd5a109088eeaf241114
Jon <jonrose08@gmail.com>
2012-05-31 11:06:25 -0700

    intit commit

---
+++ autodoc/index.clj
@@ -0,0 +1,30 @@
+{:namespaces
+ ({:source-url nil,
+   :wiki-url "clj-rsyntax.core-api.html",
+   :name "clj-rsyntax.core",
+   :author "Jon Rose",
+   :doc
+   "Wrapper API for org.fife.ui.rtextarea.RTextArea (http://fifesoft.com/) the\n\topensource text editor  component from RText."}),
+ :vars
+ ({:arglists ([rta line color]),
+   :name "add-line-highlight!",
+   :namespace "clj-rsyntax.core",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "/clj-rsyntax.core-api.html#clj-rsyntax.core/add-line-highlight!",
+   :doc "Adds a line highlight",
+   :var-type "function",
+   :line 9,
+   :file "src/clj_rsyntax/core.clj"}
+  {:arglists ([rta]),
+   :name "begin-atomic-edit!",
+   :namespace "clj-rsyntax.core",
+   :source-url nil,
+   :raw-source-url nil,
+   :wiki-url
+   "/clj-rsyntax.core-api.html#clj-rsyntax.core/begin-atomic-edit!",
+   :doc "Begins an \"atomic edit.\"",
+   :var-type "function",
+   :line 14,
+   :file "src/clj_rsyntax/core.clj"})}
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-rsyntax "0.0.1-SNAPSHOT"
+  	:description "Wrapper library for RSyntaxText components"
+  	:dependencies [[org.clojure/clojure "1.3.0"]
+                 [com.fifesoft/rsyntaxtextarea "2.0.2"]]
+    :dev-dependencies [[lein-autodoc "0.9.0"]]
+   	:autodoc 	{:name "clj-rsyntax" :page-title "RTextArea wrappers"})
